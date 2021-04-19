878486f284e57e198077a2f2eac06175ed21946b
sunil garg <snl.garg@yahoo.com>
2021-04-19 21:40:35 +0530

    Add files via upload
    Initial Project Skeleton structure checkin taken form start.spring.io

---
+++ CoronaHelper/src/main/java/io/wander/CoronaHelper/CoronaHelperApplication.java
@@ -0,0 +1,13 @@
+package io.wander.CoronaHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaHelperApplication.class, args);
+	}
+
+}
